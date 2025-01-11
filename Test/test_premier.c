4bca43a28d2e7300711aedcc21294becb5ce973f
ZokiSRB <Zokisrbija@hotmail.fr>
2025-01-11 19:25:03 +0100

    Correction du fichier

--- Test/test_premier.c
+++ Test/test_premier.c
@@ -17,8 +17,8 @@ void test_premier(void) {
 
 int main(){
     CU_initialize_registry();
-    CU_pSuite suite = CU_add_suite("Suite_", NULL, NULL);
-    CU_add_test(suite, "test_", test_premier);
+    CU_pSuite suite = CU_add_suite("Suite_premier", NULL, NULL);
+    CU_add_test(suite, "test_premier", test_premier);
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
