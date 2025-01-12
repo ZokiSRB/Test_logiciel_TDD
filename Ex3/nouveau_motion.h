9494d5afd3de509fb28f9f4e375aef6ea55aa8c9
ZokiSRB <Zokisrbija@hotmail.fr>
2025-01-12 18:46:46 +0100

    Creation du fichier .h pour l'ex3

---
+++ Ex3/nouveau_motion.h
@@ -0,0 +1,13 @@
+// motion_processing.h
+#ifndef MOTION_PROCESSING_H
+#define MOTION_PROCESSING_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define DEFAULT_SD_THRESHOLD 2
+#define MIN_SURFACE_SIZE 50
+#define MAX_SURFACE_SIZE 100000
+#define MAX_ROIS_FIRST_PASS 65536
+
+#endif
